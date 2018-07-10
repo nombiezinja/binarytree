c4ee1576a0edcf2346e6a1f7d3995510154e04a5
nombiezinja <tianyizhang1987@gmail.com>
2018-07-10 13:22:05 -0700

    Adds FloatKey to comparable

--- comparable.go
+++ comparable.go
@@ -1,17 +1,18 @@
 package binarytree
 
-import(
-  "bytes"
+import (
+	"bytes"
 )
+
 // Comparable is an interface for comparable types. All keys used in this implementation of
 // a binary tree must implement this interface.
 //
 // Helper types are supplied for some primitives, please see `IntKey`, [StringKey] and [ByteSliceKey]
 type Comparable interface {
-  LessThan(Comparable) bool
-  EqualTo(Comparable) bool
-  GreaterThan(Comparable) bool
-  ValueOf() interface{}
+	LessThan(Comparable) bool
+	EqualTo(Comparable) bool
+	GreaterThan(Comparable) bool
+	ValueOf() interface{}
 }
 
 // IntKey is a type of base type int that implements the Comparable interface.
@@ -19,22 +20,45 @@ type IntKey int
 
 // Return true if this key is less than the supplied IntKey.
 func (me IntKey) LessThan(other Comparable) bool {
-  return me < other.(IntKey)
-} 
+	return me < other.(IntKey)
+}
 
 // Return true if this key is equal to the supplied IntKey.
 func (me IntKey) EqualTo(other Comparable) bool {
-  return me == other.(IntKey)
-} 
+	return me == other.(IntKey)
+}
 
 // Return true if this key is greater than the supplied IntKey.
 func (me IntKey) GreaterThan(other Comparable) bool {
-  return me > other.(IntKey)
-} 
+	return me > other.(IntKey)
+}
 
 // Return the int value as an interface
 func (me IntKey) ValueOf() interface{} {
-  return int(me)
+	return int(me)
+}
+
+// IntKey is a type of base type int that implements the Comparable interface.
+type FloatKey int
+
+// Return true if this key is less than the supplied IntKey.
+func (me FloatKey) LessThan(other Comparable) bool {
+	return me < other.(FloatKey)
+}
+
+// Return true if this key is equal to the supplied FloatKey.
+func (me FloatKey) EqualTo(other Comparable) bool {
+	return me == other.(FloatKey)
+}
+
+// Return true if this key is greater than the supplied FloatKey.
+func (me FloatKey) GreaterThan(other Comparable) bool {
+	return me > other.(FloatKey)
+}
+
+// Return the int value as an interface
+func (me FloatKey) ValueOf() interface{} {
+	return float64(me)
 }
 
 // StringKey is a type of base type String that implements the Comparable interface.
@@ -42,22 +66,22 @@ type StringKey string
 
 // Return true if this key is less than the supplied StringKey.
 func (me StringKey) LessThan(other Comparable) bool {
-  return me < other.(StringKey)
-} 
+	return me < other.(StringKey)
+}
 
 // Return true if this key is equal to the supplied StringKey.
 func (me StringKey) EqualTo(other Comparable) bool {
-  return me == other.(StringKey)
-} 
+	return me == other.(StringKey)
+}
 
 // Return true if this key is greater than the supplied StringKey.
 func (me StringKey) GreaterThan(other Comparable) bool {
-  return me > other.(StringKey)
-} 
+	return me > other.(StringKey)
+}
 
 // Return the string value as an interface
 func (me StringKey) ValueOf() interface{} {
-  return string(me)
+	return string(me)
 }
 
 // StringKey is a type of base type String that implements the Comparable interface.
@@ -65,28 +89,37 @@ type ByteSliceKey []byte
 
 // Return true if this key is less than the supplied ByteSliceKey.
 func (me ByteSliceKey) LessThan(other Comparable) bool {
-  if len(me) > len(other.(ByteSliceKey)) { return false }
-  if len(me) < len(other.(ByteSliceKey)) { return true }
+	if len(me) > len(other.(ByteSliceKey)) {
+		return false
+	}
+	if len(me) < len(other.(ByteSliceKey)) {
+		return true
+	}
 
-  return bytes.Compare(me.ValueOf().([]byte), other.ValueOf().([]byte)) < 0
-} 
+	return bytes.Compare(me.ValueOf().([]byte), other.ValueOf().([]byte)) < 0
+}
 
 // Return true if this key is equal to the supplied ByteSliceKey.
 func (me ByteSliceKey) EqualTo(other Comparable) bool {
-  if len(me) != len(other.(ByteSliceKey)) { return false }
-  return bytes.Compare(me.ValueOf().([]byte), other.ValueOf().([]byte)) == 0
-} 
+	if len(me) != len(other.(ByteSliceKey)) {
+		return false
+	}
+	return bytes.Compare(me.ValueOf().([]byte), other.ValueOf().([]byte)) == 0
+}
 
 // Return true if this key is greater than the supplied ByteSliceKey.
 func (me ByteSliceKey) GreaterThan(other Comparable) bool {
-  if len(me) < len(other.(ByteSliceKey)) { return false }
-  if len(me) > len(other.(ByteSliceKey)) { return true }
+	if len(me) < len(other.(ByteSliceKey)) {
+		return false
+	}
+	if len(me) > len(other.(ByteSliceKey)) {
+		return true
+	}
 
-  return bytes.Compare(me.ValueOf().([]byte), other.ValueOf().([]byte)) > 0
-} 
+	return bytes.Compare(me.ValueOf().([]byte), other.ValueOf().([]byte)) > 0
+}
 
 // Return the []byte value as an interface
 func (me ByteSliceKey) ValueOf() interface{} {
-  return []byte(me)
+	return []byte(me)
 }
-
